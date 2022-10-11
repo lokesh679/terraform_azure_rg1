ffb897a905cf6a1ee0eefc5007ddacd61cc81b94
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 19:57:20 +0530

    Update main.tf
    updated terraform configuration file (main.tf) to create azure resource group

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "azurerm" "rg" {
+  features{}
+}
 
+resource "azurerm_resource_group" "rg" {
+   name = var.name
+   location = var.location
+  
+  tags = {
+     Environment = var.env
+     Team        = var.team_tags
+     Region      = var.location
+     Creator     = var.creator
+  }
+}
