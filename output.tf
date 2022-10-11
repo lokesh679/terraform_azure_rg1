d31926826c2f205a67bda869e91ca1d6f8b4079f
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 20:01:43 +0530

    Update output.tf
    Created resource group values are stored in the Output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,9 @@
+output "az_rg_id" {
+  description = "The Resource ID of the resource group"
+  value       = "${azurerm_resource_group.az_rg_id.id}"
+}
 
+output "az_rg_name" {
+  description = "The Resource Group name"
+  value       = "${var.name}
+}
