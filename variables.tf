4496b74f97fc1096454edf4cb6c2bd1d4f0e5bcc
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 19:52:50 +0530

    Update variables.tf
    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
+variables "name" {
+  description = Required name of the newly created resource group
+  default     = "cloud"
+}
+
+variables "location" {
+  description = Location of the resource group
+  default     = "westus"
+}
+
+variables "env" {
+  description = Name of the environment
+  default     = "Dev"
+}
+
+variables "team_tags" {
+  description = (optional) team tags
+  default     = "DevOps"
+}
+
+variables "creator" {
+  description = (optional) name of the creator
+  default     = "Lokesh"
+}
+
 
