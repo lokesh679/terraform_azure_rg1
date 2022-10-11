42344ebb79af8cabdb7ce6aeb8159f02ff087f2c
lokesh7devops <115445012+lokesh7devops@users.noreply.github.com>
2022-10-11 20:03:08 +0530

    Update variables.tf
    updated variables terraform file

--- variables.tf
+++ variables.tf
@@ -1,25 +1,25 @@
 variables "name" {
-  description = Required name of the newly created resource group
+  description = "Required name of the newly created resource group"
   default     = "cloud"
 }
 
 variables "location" {
-  description = Location of the resource group
+  description = "Location of the resource group"
   default     = "westus"
 }
 
 variables "env" {
-  description = Name of the environment
+  description = "Name of the environment"
   default     = "Dev"
 }
 
 variables "team_tags" {
-  description = (optional) team tags
+  description = "(optional) team tags"
   default     = "DevOps"
 }
 
 variables "creator" {
-  description = (optional) name of the creator
+  description = "(optional) name of the creator"
   default     = "Lokesh"
 }
 
